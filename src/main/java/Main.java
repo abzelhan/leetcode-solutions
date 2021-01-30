7d1ad4de20282650d6bd6ef61f5069f9e5ae941a
abzelhan <abzalsahitov@gmail.com>
2021-01-30 16:16:20 +0600

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            return null;
+        }
+    }
+
+}
